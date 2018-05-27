0fab483af27e567f7cad311fe6179f1e02ed28c4
Felixsie <31508904+Felixsie@users.noreply.github.com>
2018-05-27 05:57:36 -0700

    Complete Practical 4

---
+++ prac_05/lists_to_dict.py
@@ -0,0 +1,18 @@
+#CP1404 Practical Week 5
+
+NUMBER_OF_PEOPLE = 1
+names_of_people = []
+dobs = []
+
+
+for i in range(NUMBER_OF_PEOPLE):
+    name_input = input("Enter {} name: ".format(i + 1))
+    names_of_people.append(name_input)
+    dob_input = input("Enter {} date of birth (d/m/y): ".format(i + 1))
+    parts = dob_input.split("/")
+    one_dob = tuple([int(part) for part in parts])
+    dobs.append(dob_input)
+
+
+
+print("{} = {}".format(names_of_people, dobs))
